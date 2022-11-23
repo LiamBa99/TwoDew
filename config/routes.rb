4f61f56f69bac689bfb763d32362a27459058c9f
Liam <liambadge@gmail.com>
2022-11-23 15:38:26 -0800

    created tasks model and tasks db table

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   
-  get "/list", to: "list#index"
+  
+  get "/list", to: "lists#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
